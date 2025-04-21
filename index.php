ec465c7700a1225d8917ad6bad1140bd119fcb05
unknown <puteriocthavia.s@gmail.com>
2025-04-21 17:14:47 +0800

    edit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include "koneksi.php";
 
-$query = "SELECT * FROM mahasiswa";
+$query = "SELECT m.*, p.nama namaProdi FROM mahasiswa m JOIN prodi p ON m.id_prodi = p.id";
 $data = ambildata($query);
 
 ?>
@@ -24,6 +24,11 @@ $data = ambildata($query);
             <th>No</th>
             <th>Nim</th>
             <th>Nama</th>
+            <th>Tanggal Lahir</th>
+            <th>Telepon</th>
+            <th>Email</th>
+            <th>Prodi</th>
+            <th>Aksi</th>
         </thead>
         <tbody>
 
@@ -37,6 +42,8 @@ $data = ambildata($query);
                 <td><?php echo $d["tanggal_lahir"] ?></td>
                 <td><?php echo $d["telp"] ?></td>
                 <td><?php echo $d["email"] ?></td>
+                <td><?php echo $d["namaProdi"] ?></td>
+                <td><a href="deletemahasiswa.php?nim=<?= $d['nim']; ?>"onclick="return confirm('Yakin Ingin Hapus?')">Delete</a> | <a href="editmahasiswa.php?nim=<?= $d['nim']; ?>">Edit</a></td>
             </tr>
         <?php endforeach; ?>
 
